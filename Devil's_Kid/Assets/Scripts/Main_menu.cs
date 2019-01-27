2291efb26f008a601afc0c74de3b8a5420a54271
kreso42 <45456795+kreso42@users.noreply.github.com>
2019-01-27 02:56:10 +0100

    aaa

---
+++ Devil's_Kid/Assets/Scripts/Main_menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_menu : MonoBehaviour
+{
+    public GameObject x;
+    public GameObject y;
+    
+    public void Activate()
+    {
+        x.SetActive(false);
+        y.SetActive(true);
+    }
+}
